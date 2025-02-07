0bca89deaffce8ed0fde9b476e80b48359ef29c1
sdk <140381349+sdk66@users.noreply.github.com>
2025-02-07 10:17:50 +0800

    修改底层侧灯文件，方便自定义侧灯灯效

--- keyboards/rk/b98_3m/post_rules.mk
+++ keyboards/rk/b98_3m/post_rules.mk
@@ -2,4 +2,4 @@ RGB_MATRIX_CUSTOM_USER = yes
 RGBLIGHT_DRIVER = custom
 include keyboards/rk/b98_3m/rgb_record/rgb_record.mk
 include keyboards/rk/b98_3m/wls/wls.mk
-include keyboards/westberry/wireless/wireless.mk
+include keyboards/linker/wireless/wireless.mk
